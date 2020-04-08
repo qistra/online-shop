f292a1c2d996e970c081c88bc4dde4486bb33c61
Qi Zhang <qi.zhang.extern@pyur.com>
2020-04-08 22:23:05 +0200

    fix file indexing (.gitignore)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAkbpDL3A153e79hLBftPZPaqPZTblih3c",
+    authDomain: "online-shop-55926.firebaseapp.com",
+    databaseURL: "https://online-shop-55926.firebaseio.com",
+    projectId: "online-shop-55926",
+    storageBucket: "online-shop-55926.appspot.com",
+    messagingSenderId: "602797750655",
+    appId: "1:602797750655:web:3a0cfb2ca7a70ed785c24a",
+    measurementId: "G-TBX7QVW4KN"
+  }
 };
