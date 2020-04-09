52968613f6461123d0409abddc94f4cbafbc2493
Qi <>
2020-04-09 17:56:20 +0200

    add a cancel button in the product form; adjust css to beautify buttons in the product form

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -40,6 +40,10 @@ export class ProductFormComponent implements OnInit {
     this.router.navigate(['/admin/products']);
   }
 
+  cancel() {
+    this.router.navigate(['/admin/products']);
+  }
+
   delete() {
     if (!confirm('Are you sure you want to delete this product?')) return;
 
