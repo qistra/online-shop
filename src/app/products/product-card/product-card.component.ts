569741ba64464765172b81186b5e78e7914698dc
Qi <>
2020-04-12 19:29:13 +0200

    adjust product-card component so that it is also suitable for product-form component

--- src/app/products/product-card/product-card.component.ts
+++ src/app/products/product-card/product-card.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProductCardComponent implements OnInit {
   @Input('product') p;
+  @Input('show-button') showButton;
 
   constructor() { }
 
